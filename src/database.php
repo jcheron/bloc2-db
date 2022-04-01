5852c43f7d071285478ff7fcff646cd474bf9d20
J-C HERON <myaddressmail@gmail.com>
2022-04-01 07:53:24 +0200

    end

--- src/database.php
+++ src/database.php
@@ -1,20 +1,63 @@
 <?php
-/**
- * Establish a connection to a database
- * @param string $db the db connection
- * @param string $user the db user
- * @param string $password the db password
- * @return PDO the pdo instance
- */
-function dbConnect(string $db,string $user='root',string $password=''):PDO {
-    $pdo = new PDO("mysql:host=localhost;dbname=$db",$user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $pdo;
-}
+class Database
+{
 
-function deleteFrom(PDO $db,string $table,string $condition){
-    $sql="DELETE FROM `$table` WHERE $condition";
-    return $db->exec($sql);
-}
+    private PDO $pdo;
 
+    /**
+     * Establish a connection to a database
+     * @param string $db the db connection
+     * @param string $user the db user
+     * @param string $password the db password
+     * @return PDO the pdo instance
+     */
+    public function connect(string $db, string $user = 'root', string $password = ''): PDO
+    {
+        $this->pdo = new PDO("mysql:host=localhost;dbname=$db", $user, $password);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $this->pdo;
+    }
+
+    public function delete(string $table, string $condition)
+    {
+        $sql = "DELETE FROM `$table` WHERE $condition";
+        return $this->pdo->exec($sql);
+    }
+
+    /**
+     * Adds a new row in tableName.
+     * @param String $tableName The table name
+     * @param array $fieldValues An associative array of field names and values.
+     * i.e. ['name'=>'Smith','age'=>20,'active'=>true]
+     * @return int The inserted id
+     */
+    public function insert(String $tableName,array $fieldValues):int{
+        $fieldNamesStr='';
+        $fieldValuesStr='';
+        foreach ($fieldValues as $fieldName=>$fieldValue){
+            $fieldNamesStr.="`$fieldName`,";
+            $fieldValuesStr.="'$fieldValue',";
+        }
+        $fieldNamesStr=rtrim($fieldNamesStr,',');
+        $fieldValuesStr=rtrim($fieldValuesStr,',');
+        $sql="INSERT INTO `$tableName`($fieldNamesStr) VALUES($fieldValuesStr)";
+        $this->pdo->exec($sql);
+        return $this->pdo->lastInsertId();
+    }
+
+    /**
+     * Updates a row in $tableName.
+     * @param String $tableName The table name
+     * @param array $fieldValues The new field values
+     * @param array $keyValues The pk field name(s) and value(s) ie. ['id'=>5]
+     * @return int
+     */
+    public function update(String $tableName,array $fieldValues,array $keyValues):int{
+
+    }
+
+    public function query(string $sql):PDOStatement{
+        return $this->pdo->query($sql);
+    }
+}
 
--- test.php
+++ test.php
@@ -1,19 +1,27 @@
 <?php
 include_once 'src/database.php';
 
-$db=dbConnect('groups');
+$db=new Database();
+$db->connect('groups');
 
+if($db->insert("Organization",['name'=>'CaenSup2','domain'=>'caensup2.fr'])){
+    echo 'Organisation ajoutée !';
+};die();
 $st=$db->query('SELECT COUNT(*) FROM `User`');
 echo $st->fetchColumn(0);
 echo "<hr>";
-$st=$db->query('SELECT * FROM `User`');
-/*foreach ($st as $row){
-    echo $row['firstname']."<br>";
-}*/
 
+
+$st=$db->query('SELECT * FROM `User`');
+foreach ($st as $row){
+    echo $row['firstname']."<br>";
+}
+
+/*
 while($row=$st->fetch()){
     echo $row[1]."<br>";
 }
+*/
 
 
 /*
